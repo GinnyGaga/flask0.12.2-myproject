0ba13e5a38db3cdb9f9d1af7b37e9e5dd4a2c36d
ginny <pig@ginny.com>
2017-07-08 13:54:02 +0800

    Unique URLs / Redirection Behavior

---
+++ hello-3.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app=Flask(__name__)
+	
+@app.route('/')
+def hello():
+	return 'hello,world'
+
+@app.route('/projects/')
+def projects():
+	return 'The project page'
+
+@app.route('/about')
+def about():
+	return 'The about page'
